c0d764c9bd7850a21575a6b76e6830cee7c38e51
Demo <demodemowhitebig@gmail.com>
2018-05-10 11:05:03 -0400

    Update Stacking.py

--- trainModels/Stacking.py
+++ trainModels/Stacking.py
@@ -1,40 +1,141 @@
 #general import 
-import pandas as pd 
 import numpy as np 
-import matplotlib.pyplot as plt 
+import pandas as pd 
+matplotlib.pyplot as plt 
+import gc 
+from time import time 
+from contextlib import contextmanager 
 
-#read data
-path = '~/downloads/Dataset/'
+#def timer function 
+@contextmanager 
+def timer(operationName):
+	t0 = time()
+	yield 
+	print("{} uses {:.1f} seconds".format(operationName, time()-t0))
+
+#read data 
+path = '~/Downloads/Dataset'
 
 train_cols = ['ip', 'app', 'device', 'os', 'channel', 'click_time', 'is_attributed']
 test_cols = ['ip', 'app', 'device', 'os', 'channel', 'click_time']
+
 dtypes = {
-    'ip':'uint32',
-    'app':'uint16',
-    'device':'uint16',
-    'os':'uint16',
-    'channel':'uint16',
-    'click_id':'uint32',
-    'is_attributed':'uint8'
+	'ip': 'uint32',
+    'app': 'uint16',
+    'device': 'uint16',
+    'os': 'uint16',
+    'channel': 'uint16',
+    'is_attributed': 'uint8'
 }
 
-train = pd.read_csv(path+'train_sample.csv', dtype=dtypes, usecols=train_cols)
-test = pd.read_csv(path+'test.csv', dtype=dtypes, usecols=test_cols)
-submission = pd.read_csv(path+'sample_submission.csv')
+with timer('read training data'):
+	train = pd.read_csv(path+'train.csv', 
+		usecols=train_cols, 
+		dtype=dtypes,
+		parse_dates=['click_time'])
 
-#extract hour and day as new feature 
-train['hour'] = pd.to_datetime(train.click_time).dt.hour.astype('uint8')
-train['dayofweek'] = pd.to_datetime(train.click_time).dt.dayofweek.astype('uint8')
-train.drop(['click_time'], axis=1, inplace=True)
+with timer('read test data'):
+	test = pd.read_csv(path+'test.csv',
+		usecols=test_cols,
+		dtype=dtypes,
+		parse_dates=['click_time'])
 
-#drop ip as ip doesn't make sense here 
-train.drop(['ip'], axis=1, inplace=True)
+with timer('read submission file'):
+    submission = pd.read_csv(path+'sample_submission.csv')
 
-test['hour'] = pd.to_datetime(test.click_time).dt.hour.astype('uint8')
-test['dayofweek'] = pd.to_datetime(test.click_time).dt.dayofweek.astype('uint8')
-test.drop(['click_time'], axis=1, inplace=True)
+#extract 'day' and 'hour' from feature 'click_time' 
+train['day'] = train['click_time'].dt.day.astype('uint8')
+train['hour'] = train['click_time'].dt.hour.astype('uint8')
 
-test.drop(['ip'], axis=1, inplace=True)
+test['day'] = test['click_time'].dt.day.astype('uint8')
+test['hour'] = test['click_time'].dt.hour.astype('uint8')
+
+#create new feature by groupby and aggregation 
+groupby_list = [
+	#variance per combination of features
+	{'groupby': ['ip','app','channel'], 'select': 'day', 'agg': 'var'},
+	{'groupby': ['ip','app','os'], 'select': 'hour', 'agg': 'var'},
+	{'groupby': ['ip','day','channel'], 'select': 'hour', 'agg', 'var'},
+	
+	#count per combination of features
+	{'groupby': ['ip','day','hour'], 'select': 'channel', 'agg': 'count'},
+	{'groupby': ['ip','app'], 'select': 'channel', 'agg': 'count'},
+	{'groupby': ['ip','app','os'], 'select': 'channel', 'agg': 'count'},
+	{'groupby': ['ip','app','day','hour'], 'select': 'channel', 'agg': 'count'}
+	
+	#mean per combination of feature
+    {'groupby': ['ip','app','channel'], 'select': 'hour', 'agg': 'mean'}, 
+    
+    #average clicks by distinct users for each app
+    {'groupby': ['ip'], 'select': 'ip', 'agg': lambda x: len(x)/len(x.unique()),
+    'agg_name': 'avgViewDist'},
+    
+    #how many unique counts per ip and combination of features
+    {'groupby': ['ip'], 'select': 'channel', 'agg': 'nunique'}, 
+    {'groupby': ['ip'], 'select': 'app', 'agg': 'nunique'}, 
+    {'groupby': ['ip','day'], 'select': 'hour', 'agg': 'nunique'}, 
+    {'groupby': ['ip','app'], 'select': 'os', 'agg': 'nunique'}, 
+    {'groupby': ['ip'], 'select': 'device', 'agg': 'nunique'}, 
+    {'groupby': ['app'], 'select': 'channel', 'agg': 'nunique'}, 
+    {'groupby': ['ip'], 'select': 'os', 'agg': 'nunique'}, 
+    
+    #how many cumulative count of app per ip and other features
+    {'groupby': ['ip','device','os'], 'select': 'app', 'agg': 'cumcount'}, 
+    {'groupby': ['ip'], 'select': 'app', 'agg': 'cumcount'}
+	]
+
+with timer('create groupby features'):
+    for item in groupby_list:
+	    name = item['agg_name'] if 'agg_name' in item else item['agg']
+	    feature_name = '{}_{}_{}'.format('_'.join(item['groupby']), name, item['select'])
+	
+	    used_features = list(set(item['groupby'] + item['select']))
+	
+	    temp = train[used_features].\
+	        groupby(item['groupby'])[item['select']].\
+	        agg(item['agg']).\
+	        reset_index().\
+	        rename(index=str, columns={item['select']: feature_name})
+	
+	    temp1 = test[used_features].\
+	        groupby(item['groupby'])[item['select']].\
+	        agg(item['agg']).\
+	        reset_index().\
+	        rename(index=str, columns={item['select']: feature_name})
+	
+	    if item['agg'] == 'cumcount':
+		    train[feature_name] = temp[0].values
+		    test[feature_name] = temp1[0].values
+	    else:
+		    train = train.merge(temp, on=item['groupby'], how='left')
+		    test = test.merge(temp1, on=item['groupby'], how='left')
+
+	    del temp, temp1
+	    gc.collect()
+        
+#how long it takes for a next click?
+with timer('create next_click feature'):
+	train['next_click'] = train[['ip','os','device','app','click_time']].\
+		groupby(['ip','os','device','app']).\
+		click_time.\
+		transform(lambda x: x.diff().shift(-1)).\
+		dt.seconds
+
+	test['next_click'] = train[['ip','os','device','app','click_time']].\
+		groupby(['ip','os','device','app']).\
+		click_time.\
+		transform(lambda x: x.diff().shift(-1)).\
+		dt.seconds
+
+train.drop('click_time', axis=1, inplace=True)
+test.drop('click_time', axis=1, inplace=True)
+
+gc.collect()
+
+
+    
+    
+    
 
 #train a neural net classifier 
 from sklearn.neural_network import MLPClassifier
